9d7304ec33445fcb2b07c4a5d30d4005d225f0e8
Alrzamini <mranotmillion@gmail.com>
2023-01-06 21:51:28 +0330

    add log model and repository interface

---
+++ AloTaxi.Domain/Entities/Log/LogModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AloTaxi.Domain.Entities.Base;
+
+namespace AloTaxi.Domain.Entities.Log
+{
+    public class LogModel : BaseEntity
+    {
+        [MaxLength(1000)]
+        public string? Message { get; set; }
+
+        [Required]
+        public LogLevel Level { get; set; }
+    }
+
+    public enum LogLevel
+    {
+        High,
+        Medium,
+        Low
+    }
+}
---
+++ AloTaxi.Domain/Interfaces/Log/ILoggerRepository.cs
@@ -0,0 +1,8 @@
+﻿using AloTaxi.Domain.Entities.Log;
+
+namespace AloTaxi.Domain.Interfaces.Log;
+
+public interface ILoggerRepository
+{
+    public Task LogInformation(LogModel model);
+}
